e7b72b5c37e63e8134070d768b0dcc23432c3ebe
https://github.com/sidocoder/first-intern <sifhoran2024@gmail.com>
2025-07-11 17:29:29 +0300

    final commit

--- script.js
+++ script.js
@@ -4,12 +4,44 @@ function addTask() {
   if (taskText === "") return;
 
   const li = document.createElement("li");
-  li.textContent = taskText;
 
-  li.onclick = function () {
-    this.remove(); 
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  const span = document.createElement("span");
+  span.textContent = taskText;
+
+  checkbox.onchange = function () {
+    span.classList.toggle("checked", this.checked);
   };
 
+//edit button
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.className = "edit-btn";
+  editBtn.onclick = function () {
+    const updatedText = prompt("Edit your task:", span.textContent);
+    if (updatedText !== null && updatedText.trim() !== "") {
+      span.textContent = updatedText.trim();
+    }
+  };
+
+  // delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.className = "delete-btn";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(editBtn);
+  li.appendChild(deleteBtn);
+
+
   document.getElementById("taskList").appendChild(li);
+
   taskInput.value = "";
 }
